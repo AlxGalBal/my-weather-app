fab33579f3a4def1a4b68a255f74b1d832267abf
AlxGalBal <79552448+AlxGalBal@users.noreply.github.com>
2021-04-04 18:00:51 -0500

    Created JS file

---
+++ src/index.js
@@ -0,0 +1,59 @@
+let now = new Date();
+let dayAndHour = document.querySelector(".city-hour");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+let hour = now.getHours();
+if (hour < 10) {
+  hour = `0${hour}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+dayAndHour.innerHTML = `${day}, ${hour}:${minutes} hours`;
+
+function showWeather(response) {
+  let cityName = document.querySelector("#searched-city");
+  cityName.innerHTML = response.data.name;
+  let currentCityTemp = document.querySelector("#todays-temperature");
+  currentCityTemp.innerHTML = Math.round(response.data.main.temp);
+}
+
+function citySearch(event) {
+  event.preventDefault();
+  let input = document.querySelector(".form-control");
+  let citySearched = document.querySelector("#searched-city");
+  citySearched.innerHTML = `${input.value}`;
+  search(input.value);
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", citySearch);
+
+function search(city) {
+  let apiKey = "5c5df4d7dda74f566375f7ab7cc86495";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function showPosition(position) {
+  let apiKey = "5c5df4d7dda74f566375f7ab7cc86495";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCurrentPosition() {
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentPosition);
